c9b1860d837915c7e2634e0b6576abf8be376a37
UPSJhona <34308770+UPSJhona@users.noreply.github.com>
2020-04-08 23:50:42 -0500

    Funciones

--- js/funciones.js
+++ js/funciones.js
@@ -27,18 +27,32 @@ function buscar(url) {
             console.log(peliculas);
             let html = ''
             peliculas.forEach(pelicula => {
+
+
                 html += `
 
                 <tr>
-                <td><img class="ui centered small rounded image raised link green card"  src="${pelicula.Poster}" alt=""></td> 
-                <i class="video icon"></i>
+                <td style="background-color:rgb(42, 44, 46)"><img class="ui centered small rounded image raised link green card"  src="${pelicula.Poster}" alt=""></td> 
+                <i class="video icon blue"></i>
                 
-                <td class="ui placeholder"><i class="file alternate icon"></i><br>${pelicula.Title} </td>
+                <td class="ui placeholder centered"><i class="file alternate icon green"></i><br><h2 style="color:green;">${pelicula.Title}</h2> </td>
+               
+                <td class="ui placeholder centered "><p class="font-size:100%";><h2>${pelicula.Year} </h2></p></td>
                 
-                <td class="ui placeholder">${pelicula.Year}</td>
-                <td class="ui placeholder">${pelicula.Type}</td>
-                <td class="ui placeholder"><a href="">Ver mas</a></td>
+                <td class="ui placeholder"><h2>${pelicula.Type}</h2></td>
+                
+
+
+                <td class="ui placeholder centered">  
+
+                 <a class="btn btn-primary btn-lg btn purple-gradient" data-toggle="modal" onclick="buscarID('${pelicula.imdbID}')">Descripcion</a>
+                 <h2 id="tablaDatosPelicula2"></h2>
+  
+                 </td>
+
             </tr>
+
+            
           
 
 
@@ -50,15 +64,63 @@ function buscar(url) {
 
 
         } else {
-           
-            
-  
+
+
+
             document.querySelector('#tablaDatosPelicula').innerHTML = "<h2>    BUSQUEDA NO ENCONTRADA.........</h2>"
-            
+
 
         }
     };
     xmlhttp.open("GET", url, true)
     xmlhttp.send()
 
+}
+
+
+
+
+function buscarID(num) {
+    console.log(num)
+
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const peliculas = JSON.parse(this.responseText)
+            console.log(peliculas);
+            let html = `
+                   
+            <div>
+               
+                    <h2 class="modal-title">${peliculas.Title}</h2>
+                    <p class="card-text">Año: ${peliculas.Year}</p>
+                    <p class="card-text">${peliculas.Rated}</p>
+                    <p class="card-text">${peliculas.Released}</p>
+                    <p class="card-text">Tiempo:  ${peliculas.Runtime}</p>
+                    <p class="card-text">Genero:  ${peliculas.Genre}</p>
+                    <p class="card-text">Director:  ${peliculas.Director}</p>
+                    <p class="card-text">Escrito:  ${peliculas.Writer}</p>
+                    <p class="card-text">Trama:  ${peliculas.Plot}</p>
+                    <p class="card-text">Lenguaje:  ${peliculas.Language}</p>
+                    <p class="card-text">Pais:  ${peliculas.Country}</p>
+                    
+               
+            
+        </div>
+    
+
+
+                `;
+
+                document.querySelector('#tablaDatosPelicula2').innerHTML = html
+        } else {
+            // document.querySelector('#ventana1').innerHTML = "<h2>No existe la pelicula.</h2>"
+        }
+    };
+    xmlhttp.open("GET", "http://www.omdbapi.com/?apikey=a5ac401a&i=" + num, true)
+    xmlhttp.send()
 }
